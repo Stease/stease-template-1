a2118db5e23182ed622520c498e91d51b0f20bb4
Eise <eise.zimmerman@outlook.com>
2021-03-05 13:28:37 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styling/index.css';
+import Homepage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
+import reportWebVitals from './reportWebVitals';
+
+if (window.location.href.toLowerCase().includes("?page=home")) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Homepage />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+} else if (window.location.href.toLowerCase().includes("?page=contact")) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <ContactPage />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Homepage />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
